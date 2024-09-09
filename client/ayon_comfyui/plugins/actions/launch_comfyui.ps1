86121de8963b8d3f985d97f0b808d592968c3e37
doerp <me@doerp.cool>
2024-09-09 20:03:29 +0200

    replaces conda with uv

--- client/ayon_comfyui/plugins/actions/launch_comfyui.ps1
+++ client/ayon_comfyui/plugins/actions/launch_comfyui.ps1
@@ -1,33 +1,22 @@
+# assumes to be in comfyui directory
 param(
     [switch]$useCpu = $false
 )
 
-# Define the name of the conda environment
-$envName = "ayon_comfyui"
-
-
-# Check if the conda environment exists
-$envExists = conda env list | Select-String -Pattern "^\s*$envName\s"
-
-if (-not $envExists) {
-    # Create the conda environment if it does not exist
-    Write-Output "Creating conda environment: $envName"
-    conda create -n $envName python -y
-} else {
-    Write-Output "Conda environment '$envName' already exists."
+# Check for local venv
+if (-not (Test-Path .\venv)) {
+    uv venv
 }
+.venv\Scripts\activate
 
-# Activate the conda environment
-conda activate $envName
-conda info
+# Install requirements
+uv pip install pip # needed by ComfyUI-Manager to install node deps
+uv pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu124
 
-# Install requirements from requirements.txt
-# use python -I -m pip instead of pip to avoid environment conflicts
-python -I -m pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu124
 
 # run the server
 if ($useCpu) {
-    python .\main.py --cpu
+    uv run .\main.py --cpu
 } else {
-    python .\main.py
+    uv run .\main.py
 }
