27eff996ef2fd67203a26bf730a3e80a3d3626ed
doerp <me@doerp.cool>
2024-09-09 21:07:17 +0200

    ruff

--- client/ayon_comfyui/custom_nodes/example_node.py
+++ client/ayon_comfyui/custom_nodes/example_node.py
@@ -33,74 +33,86 @@ class Example:
         The entry point method. The name of this method must be the same as the value of property `FUNCTION`.
         For example, if `FUNCTION = "execute"` then this method's name must be `execute`, if `FUNCTION = "foo"` then it must be `foo`.
     """
+
     def __init__(self):
         pass
 
     @classmethod
     def INPUT_TYPES(s):
         """
-            Return a dictionary which contains config for all input fields.
-            Some types (string): "MODEL", "VAE", "CLIP", "CONDITIONING", "LATENT", "IMAGE", "INT", "STRING", "FLOAT".
-            Input types "INT", "STRING" or "FLOAT" are special values for fields on the node.
-            The type can be a list for selection.
+        Return a dictionary which contains config for all input fields.
+        Some types (string): "MODEL", "VAE", "CLIP", "CONDITIONING", "LATENT", "IMAGE", "INT", "STRING", "FLOAT".
+        Input types "INT", "STRING" or "FLOAT" are special values for fields on the node.
+        The type can be a list for selection.
 
-            Returns: `dict`:
-                - Key input_fields_group (`string`): Can be either required, hidden or optional. A node class must have property `required`
-                - Value input_fields (`dict`): Contains input fields config:
-                    * Key field_name (`string`): Name of a entry-point method's argument
-                    * Value field_config (`tuple`):
-                        + First value is a string indicate the type of field or a list for selection.
-                        + Second value is a config for type "INT", "STRING" or "FLOAT".
+        Returns: `dict`:
+            - Key input_fields_group (`string`): Can be either required, hidden or optional. A node class must have property `required`
+            - Value input_fields (`dict`): Contains input fields config:
+                * Key field_name (`string`): Name of a entry-point method's argument
+                * Value field_config (`tuple`):
+                    + First value is a string indicate the type of field or a list for selection.
+                    + Second value is a config for type "INT", "STRING" or "FLOAT".
         """
         return {
             "required": {
                 "image": ("IMAGE",),
-                "int_field": ("INT", {
-                    "default": 0, 
-                    "min": 0, #Minimum value
-                    "max": 4096, #Maximum value
-                    "step": 64, #Slider's step
-                    "display": "number", # Cosmetic only: display as "number" or "slider"
-                    "lazy": True # Will only be evaluated if check_lazy_status requires it
-                }),
-                "float_field": ("FLOAT", {
-                    "default": 1.0,
-                    "min": 0.0,
-                    "max": 10.0,
-                    "step": 0.01,
-                    "round": 0.001, #The value representing the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
-                    "display": "number",
-                    "lazy": True
-                }),
+                "int_field": (
+                    "INT",
+                    {
+                        "default": 0,
+                        "min": 0,  # Minimum value
+                        "max": 4096,  # Maximum value
+                        "step": 64,  # Slider's step
+                        "display": "number",  # Cosmetic only: display as "number" or "slider"
+                        "lazy": True,  # Will only be evaluated if check_lazy_status requires it
+                    },
+                ),
+                "float_field": (
+                    "FLOAT",
+                    {
+                        "default": 1.0,
+                        "min": 0.0,
+                        "max": 10.0,
+                        "step": 0.01,
+                        "round": 0.001,  # The value representing the precision to round to, will be set to the step value by default. Can be set to False to disable rounding.
+                        "display": "number",
+                        "lazy": True,
+                    },
+                ),
                 "print_to_screen": (["enable", "disable"],),
-                "string_field": ("STRING", {
-                    "multiline": False, #True if you want the field to look like the one on the ClipTextEncode node
-                    "default": "Hello World!",
-                    "lazy": True
-                }),
+                "string_field": (
+                    "STRING",
+                    {
+                        "multiline": False,  # True if you want the field to look like the one on the ClipTextEncode node
+                        "default": "Hello World!",
+                        "lazy": True,
+                    },
+                ),
             },
         }
 
     RETURN_TYPES = ("IMAGE",)
-    #RETURN_NAMES = ("image_output_name",)
+    # RETURN_NAMES = ("image_output_name",)
 
     FUNCTION = "test"
 
-    #OUTPUT_NODE = False
+    # OUTPUT_NODE = False
 
     CATEGORY = "Example"
 
-    def check_lazy_status(self, image, string_field, int_field, float_field, print_to_screen):
+    def check_lazy_status(
+        self, image, string_field, int_field, float_field, print_to_screen
+    ):
         """
-            Return a list of input names that need to be evaluated.
+        Return a list of input names that need to be evaluated.
 
-            This function will be called if there are any lazy inputs which have not yet been
-            evaluated. As long as you return at least one field which has not yet been evaluated
-            (and more exist), this function will be called again once the value of the requested
-            field is available.
+        This function will be called if there are any lazy inputs which have not yet been
+        evaluated. As long as you return at least one field which has not yet been evaluated
+        (and more exist), this function will be called again once the value of the requested
+        field is available.
 
-            Any evaluated inputs will be passed as arguments to this function. Any unevaluated
-            inputs will have the value None.
+        Any evaluated inputs will be passed as arguments to this function. Any unevaluated
+        inputs will have the value None.
         """
         if print_to_screen == "enable":
             return ["int_field", "float_field", "string_field"]
@@ -114,7 +126,7 @@ class Example:
                 int_field: {int_field}
                 float_field: {float_field}
             """)
-        #do some processing on the image, in this example I just invert it
+        # do some processing on the image, in this example I just invert it
         image = 1.0 - image
         return (image,)
 
@@ -126,9 +138,10 @@ class Example:
         This method is used in the core repo for the LoadImage node where they return the image hash as a string, if the image hash
         changes between executions the LoadImage node is executed again.
     """
-    #@classmethod
-    #def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
+    # @classmethod
+    # def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
     #    return ""
+
 
 # Set the web directory, any .js file in that directory will be loaded by the frontend as a frontend extension
 # WEB_DIRECTORY = "./somejs"
@@ -138,6 +151,7 @@ class Example:
 from aiohttp import web
 from server import PromptServer
 
+
 @PromptServer.instance.routes.get("/hello")
 async def get_hello(request):
     return web.json_response("hello")
@@ -145,11 +159,7 @@ async def get_hello(request):
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
-NODE_CLASS_MAPPINGS = {
-    "Example": Example
-}
+NODE_CLASS_MAPPINGS = {"Example": Example}
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "Example": "Example Node"
-}
+NODE_DISPLAY_NAME_MAPPINGS = {"Example": "Example Node"}
