961456fb5269d6787110d0d3553dcccd2fd7b264
doerp <me@doerp.cool>
2024-09-09 20:36:18 +0200

    logging

--- client/ayon_comfyui/plugins/actions/launch_comfyui.ps1
+++ client/ayon_comfyui/plugins/actions/launch_comfyui.ps1
@@ -10,12 +10,14 @@ if (-not (Test-Path .\venv)) {
 .venv\Scripts\activate
 
 # Install requirements
+Write-Output "Installing ComfyUI requirements"
 uv pip install pip # needed by ComfyUI-Manager to install node deps
 uv pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu124
 
 
 # run the server
 if ($useCpu) {
+    Write-Output "Running ComfyUI with CPU"
     uv run .\main.py --cpu
 } else {
     uv run .\main.py
