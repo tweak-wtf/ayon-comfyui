cb49ab80635a7a9abc3c75083d28219013758cd5
doerp <me@doerp.cool>
2024-09-14 20:25:27 +0200

    wip: image save

--- client/ayon_comfyui/custom_nodes/publish_image.py
+++ client/ayon_comfyui/custom_nodes/publish_image.py
@@ -1,98 +1,34 @@
+import numpy
+from PIL import Image
+
+
 class PublishImage:
-    """
-    A example node
-
-    Class methods
-    -------------
-    INPUT_TYPES (dict):
-        Tell the main program input parameters of nodes.
-    IS_CHANGED:
-        optional method to control when the node is re executed.
-
-    Attributes
-    ----------
-    RETURN_TYPES (`tuple`):
-        The type of each element in the output tuple.
-    RETURN_NAMES (`tuple`):
-        Optional: The name of each output in the output tuple.
-    FUNCTION (`str`):
-        The name of the entry-point method. For example, if `FUNCTION = "execute"` then it will run Example().execute()
-    OUTPUT_NODE ([`bool`]):
-        If this node is an output node that outputs a result/image from the graph. The SaveImage node is an example.
-        The backend iterates on these output nodes and tries to execute all their parents if their parent graph is properly connected.
-        Assumed to be False if not present.
-    CATEGORY (`str`):
-        The category the node should appear in the UI.
-    DEPRECATED (`bool`):
-        Indicates whether the node is deprecated. Deprecated nodes are hidden by default in the UI, but remain
-        functional in existing workflows that use them.
-    EXPERIMENTAL (`bool`):
-        Indicates whether the node is experimental. Experimental nodes are marked as such in the UI and may be subject to
-        significant changes or removal in future versions. Use with caution in production workflows.
-    execute(s) -> tuple || None:
-        The entry point method. The name of this method must be the same as the value of property `FUNCTION`.
-        For example, if `FUNCTION = "execute"` then this method's name must be `execute`, if `FUNCTION = "foo"` then it must be `foo`.
-    """
-
     def __init__(self):
         pass
 
     @classmethod
     def INPUT_TYPES(s):
-        """
-        Return a dictionary which contains config for all input fields.
-        Some types (string): "MODEL", "VAE", "CLIP", "CONDITIONING", "LATENT", "IMAGE", "INT", "STRING", "FLOAT".
-        Input types "INT", "STRING" or "FLOAT" are special values for fields on the node.
-        The type can be a list for selection.
-
-        Returns: `dict`:
-            - Key input_fields_group (`string`): Can be either required, hidden or optional. A node class must have property `required`
-            - Value input_fields (`dict`): Contains input fields config:
-                * Key field_name (`string`): Name of a entry-point method's argument
-                * Value field_config (`tuple`):
-                    + First value is a string indicate the type of field or a list for selection.
-                    + Second value is a config for type "INT", "STRING" or "FLOAT".
-        """
         return {"required": {"image": ("IMAGE")}}
 
     RETURN_TYPES = ()
     # RETURN_NAMES = ("image_output_name",)
 
-    FUNCTION = "test"
+    FUNCTION = "main"
 
-    # OUTPUT_NODE = False
+    OUTPUT_NODE = True
 
-    CATEGORY = "bEpic"
+    CATEGORY = "👀"
 
-    def check_lazy_status(
-        self, image, string_field, int_field, float_field, print_to_screen
-    ):
-        """
-        Return a list of input names that need to be evaluated.
+    def check_lazy_status(self, image):
+        return ["image"]
 
-        This function will be called if there are any lazy inputs which have not yet been
-        evaluated. As long as you return at least one field which has not yet been evaluated
-        (and more exist), this function will be called again once the value of the requested
-        field is available.
-
-        Any evaluated inputs will be passed as arguments to this function. Any unevaluated
-        inputs will have the value None.
-        """
-        if print_to_screen == "enable":
-            return ["int_field", "float_field", "string_field"]
-        else:
-            return []
-
-    def test(self, image, string_field, int_field, float_field, print_to_screen):
-        if print_to_screen == "enable":
-            print(f"""Your input contains:
-                string_field aka input text: {string_field}
-                int_field: {int_field}
-                float_field: {float_field}
-            """)
+    def main(self, image):
         # do some processing on the image, in this example I just invert it
-        image = 1.0 - image
-        return (image,)
+        # image = numpy.array(image)
+        image = numpy.array(image)
+        img = Image.fromarray(image)
+        img.save("image.jpg")
+        # return ()
 
     """
         The node will always be re executed if any of the inputs change but
