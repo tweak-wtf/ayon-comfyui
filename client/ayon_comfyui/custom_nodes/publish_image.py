d59afe41dad853af170f444132464b9a830d5f5c
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2025-04-27 12:32:29 +0200

    init AyonNode

--- client/ayon_comfyui/custom_nodes/publish_image.py
+++ client/ayon_comfyui/custom_nodes/publish_image.py
@@ -1,60 +1,134 @@
+import logging
 import numpy
+from os import environ
+from copy import deepcopy
+from pathlib import Path
 from PIL import Image
 
+import ayon_api
 
-class PublishImage:
+log = logging.getLogger(__name__)
+
+
+class AyonNode:
     def __init__(self):
-        pass
+        ayon_env = {k: v for k, v in environ.items() if k.startswith("AYON_")}
+        self.project = ayon_env.get("AYON_PROJECT_NAME")
+        log.info(f"{self.project = }")
+        self.app = ayon_env.get("AYON_APP_NAME")
+        log.info(f"{self.app = }")
+        self.folder = ayon_env.get("AYON_FOLDER_PATH")
+        log.info(f"{self.folder = }")
+        self.task = ayon_env.get("AYON_TASK_NAME")
+        log.info(f"{self.task = }")
+
+    @property
+    def app(self) -> dict:
+        if not self.__app:
+            raise ValueError("App not set")
+        return self.__app
+
+    @app.setter
+    def app(self, name: str):
+        if "/" not in name:
+            raise ValueError("App name must be in the format 'app_name/version'")
+        app_splits = name.split("/")
+        self.__app = {
+            "name": app_splits[0],
+            "version": app_splits[1],
+        }
+
+    @property
+    def app_name(self) -> str:
+        if not self.app:
+            raise ValueError("App not set")
+        return self.app.split("/")[0]
+
+    @property
+    def app_version(self) -> str:
+        if not self.app:
+            raise ValueError("App not set")
+        return self.app.split("/")[1]
+
+    @property
+    def folder(self) -> dict:
+        if not self.__folder:
+            raise ValueError("Folder not set")
+        return self.__folder
+
+    @folder.setter
+    def folder(self, path: str):
+        if not self.project:
+            raise ValueError("Project not set")
+
+        folder_entity = ayon_api.get_folder_by_path(self.project["name"], path)
+        if not folder_entity:
+            raise ValueError(f"Folder {path} not found")
+
+        self.__folder = folder_entity
+
+    @property
+    def project(self) -> dict:
+        if not self.__project:
+            raise ValueError("Project not set")
+        return self.__project
+
+    @project.setter
+    def project(self, name: str):
+        project = ayon_api.get_project(name)
+        if not project:
+            raise ValueError(f"Project {name} not found")
+        
+        self.__project = project
+
+    @property
+    def task(self) -> dict:
+        if not self.__task:
+            raise ValueError("Task not set")
+        return self.__task
+    
+    @task.setter
+    def task(self, name: str):
+        if not self.project:
+            raise ValueError("Project not set")
+    
+        if not self.folder:
+            raise ValueError("Folder not set")
+
+        task = ayon_api.get_task_by_folder_path(
+            self.project["name"],
+            self.folder["path"],
+            name,
+        )
+        if not task:
+            raise ValueError(f"Task {name} not found")
+
+        self.__task = task
+
+class PublishImage(AyonNode):
+    """AYON PublishImage node for ComfyUI
+
+    - save 8bit png as represenation
+    - save workflow as workfile
+    - publish workfile and representation
+    """
+
+    def __init__(self):
+        super().__init__()
 
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"image": ("IMAGE")}}
+        # add save workfile bool
+        return {"required": {"image": ("IMAGE",)}}
 
     RETURN_TYPES = ()
     # RETURN_NAMES = ("image_output_name",)
-
     FUNCTION = "main"
-
     OUTPUT_NODE = True
-
     CATEGORY = "👀"
 
-    def check_lazy_status(self, image):
-        return ["image"]
-
     def main(self, image):
-        # do some processing on the image, in this example I just invert it
-        # image = numpy.array(image)
-        image = numpy.array(image)
-        img = Image.fromarray(image)
-        img.save("image.jpg")
-        # return ()
-
-    """
-        The node will always be re executed if any of the inputs change but
-        this method can be used to force the node to execute again even when the inputs don't change.
-        You can make this node return a number or a string. This value will be compared to the one returned the last time the node was
-        executed, if it is different the node will be executed again.
-        This method is used in the core repo for the LoadImage node where they return the image hash as a string, if the image hash
-        changes between executions the LoadImage node is executed again.
-    """
-    # @classmethod
-    # def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
-    #    return ""
-
-
-# Set the web directory, any .js file in that directory will be loaded by the frontend as a frontend extension
-# WEB_DIRECTORY = "./somejs"
-
-
-# Add custom API routes, using router
-from aiohttp import web
-from server import PromptServer
-
-
-@PromptServer.instance.routes.get("/hello")
-async def get_hello(request):
-    return web.json_response("hello")
+        log.info(f"{image = }")
 
 
 # A dictionary that contains all nodes you want to export with their names
