45e6569eafd966d4db86a9335b20c8086c9a0af9
SebastianZilius <sebastian.zilius@bepic.studio>
2025-08-23 20:37:40 +0200

    removed unnecessary code (async functions)

--- client/ayon_comfyui/plugins/load/apply_comfyuiWorkflow.py
+++ client/ayon_comfyui/plugins/load/apply_comfyuiWorkflow.py
@@ -3,7 +3,6 @@ from ayon_applications import ApplicationManager
 
 from ayon_core.pipeline import load
 from ayon_core.pipeline.load import LoadError
-import asyncio
 from ayon_comfyui.comfyuiclient import ComfyUIClientAsync, ComfyUIClient
 from pathlib import Path
 import qargparse
@@ -42,29 +41,15 @@ class comfyuiRMGB(load.LoaderPlugin):
         ]
     
     def comfyui_open_session(self, workflowname):
-        try:
-            server_adress = "127.0.0.1:8188"
-            self.client = ComfyUIClient(server_adress, workflowname, debug=True)
-            self.client.connect()
-        except:
-            print('Error')
-    
-    async def comfyui_open_session_async(self, workflowname):
         server_adress = "127.0.0.1:8188"
-        self.client = ComfyUIClientAsync(server_adress, workflowname, debug=True)
-        await self.client.connect()
+        self.client = ComfyUIClient(server_adress, workflowname, debug=True)
+        self.client.connect()
 
     def comfyui_setData(self, filepath_load, filepath_save, first_frame, last_frame):
         self.client.set_data(key='AYON_filename_load', value=filepath_load)
         self.client.set_data(key='AYON_filename_save', value=filepath_save)
         self.client.set_data(key='AYON_startframe', value=first_frame)
         self.client.set_data(key='AYON_endframe', value=last_frame)
-
-    async def comfyui_setData_async(self, filepath_load, filepath_save, first_frame, last_frame):
-        await self.client.set_data(key='AYON_filename_load', value=filepath_load)
-        await self.client.set_data(key='AYON_filename_save', value=filepath_save)
-        await self.client.set_data(key='AYON_startframe', value=first_frame)
-        await self.client.set_data(key='AYON_endframe', value=last_frame)
     
     def make_filepaths(self, context):
         # create in- and out filepaths, compatible with OpenEXR plugin in comfy
@@ -79,8 +64,6 @@ class comfyuiRMGB(load.LoaderPlugin):
         # get context
         version_entity = context["version"]
         version_attributes = version_entity["attrib"]
-        repre_entity = context["representation"]
-        repre_id = repre_entity["id"]
         workflowname = options.get(
             "load_workflow", self.options_defaults["load_workflow"]
         )
@@ -90,14 +73,9 @@ class comfyuiRMGB(load.LoaderPlugin):
         first_frame = version_attributes.get("frameStart")
         last_frame = version_attributes.get("frameEnd")
         #run comfyui session
-        # self.comfyui_open_session(workflowname)
-        try:
-            server_adress = "127.0.0.1:8188"
-            self.client = ComfyUIClient(server_adress, workflowname, debug=True)
-            self.client.connect()
-        except:
-            print('Error')
-        else:
-            self.comfyui_setData(filepath_load.as_posix(), filepath_save.as_posix(), first_frame, last_frame)
-            self.client.generate()
-            self.client.close()
+        server_adress = "127.0.0.1:8188"
+        self.client = ComfyUIClient(server_adress, workflowname, debug=True)
+        self.client.connect()
+        self.comfyui_setData(filepath_load.as_posix(), filepath_save.as_posix(), first_frame, last_frame)
+        self.client.generate()
+        self.client.close()
